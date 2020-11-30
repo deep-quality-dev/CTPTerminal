4c22221062e6a4afe6c0486982354acdb2279cbc
goldstar081 <goldstar081@gmail.com>
2020-11-30 16:27:33 +0800

    .

---
+++ ApiLib/Utils/Logger.cpp
@@ -0,0 +1,22 @@
+﻿#include "Logger.h"
+#include "Utils/Utils.h"
+#include <iostream>
+#include "log4cplus/logger.h"
+
+void Utils::Log(const std::string& data, bool to_console, int log_level /*= log4cplus::INFO_LOG_LEVEL*/)
+{
+	if (to_console) {
+		std::cout << GetCurrentDateTime() << " [INFO] " << data << std::endl;
+	}
+	if (log_level >= log4cplus::INFO_LOG_LEVEL)
+		LOG4CPLUS_INFO(log4cplus::Logger::getInstance(L"Test1"), ConvertMultibyte2Unicode(data.c_str()));
+}
+
+void Utils::Log(const std::wstring& data, bool to_console, int log_level /*= log4cplus::INFO_LOG_LEVEL*/)
+{
+	if (to_console) {
+		std::cout << GetCurrentDateTime() << " [INFO] " << ConvertUnicode2Multibyte(data.c_str()).c_str() << std::endl;
+	}
+	if (log_level >= log4cplus::INFO_LOG_LEVEL)
+		LOG4CPLUS_INFO(log4cplus::Logger::getInstance(L"Test1"), data.c_str());
+}
---
+++ ApiLib/Utils/Logger.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <string.h>
+#include "log4cplus/loglevel.h"
+
+namespace Utils {
+
+	void Log(const std::string& data, bool to_console, int log_level = log4cplus::INFO_LOG_LEVEL);
+	void Log(const std::wstring& data, bool to_console, int log_level = log4cplus::INFO_LOG_LEVEL);
+}
