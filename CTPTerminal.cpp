6dbc31530fb85e8b000d4e5995b15646c36613df
goldstar081 <goldstar081@gmail.com>
2020-12-08 13:34:48 +0800

    .

--- CTPTerminal.cpp
+++ CTPTerminal.cpp
@@ -13,12 +13,11 @@ int main()
 
 	// 在行情终端，只能手动设置合约列表
 	std::set<Instrument> instruments;
-	instruments.insert(Instrument(ExchangeID::SHFE, "au2012"));
-	instruments.insert(Instrument(ExchangeID::SHFE, "au2101"));
-	instruments.insert(Instrument(ExchangeID::SHFE, "au2102"));
-	instruments.insert(Instrument(ExchangeID::SHFE, "ag2012"));
-	instruments.insert(Instrument(ExchangeID::SHFE, "ag2101"));
-	instruments.insert(Instrument(ExchangeID::SHFE, "ag2102"));
+	Instrument au2012(ExchangeID::SHFE, "au2012"); au2012.product_id = "au"; instruments.insert(au2012);
+	Instrument au2101(ExchangeID::SHFE, "au2101"); au2101.product_id = "au"; instruments.insert(au2101);
+	Instrument ag2012(ExchangeID::SHFE, "ag2012"); au2101.product_id = "au"; instruments.insert(ag2012);
+	Instrument ag2012(ExchangeID::SHFE, "ag2012"); au2101.product_id = "au"; instruments.insert(ag2012);
+	Instrument ag2102(ExchangeID::SHFE, "ag2102"); au2101.product_id = "au"; instruments.insert(ag2102);
 
 	quote_service.SetInstruments(instruments);
 	quote_service.data_center()->OnRtnInstruments(instruments);
